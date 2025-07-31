a67debada0b33e77123c52bb6f90a2b8409bec8a
JWhyFR <54511523+JWhyFR@users.noreply.github.com>
2025-07-31 18:44:31 +0200

    Update clean.php
    pour bascule UTF8

--- ejp_api/clean.php
+++ ejp_api/clean.php
@@ -3,7 +3,7 @@
 // Utiliser glob pour trouver tous les fichiers correspondant au motif ejp[0-9][0-9].txt
 $fichiers = glob('ejp[0-9][0-9].txt');
 
-// Parcourir chaque fichier trouvé et le supprimer
+// Parcourir chaque fichier trouvÃ© et le supprimer
 foreach ($fichiers as $fichier) {
     if (is_file($fichier)) {
         unlink($fichier);
@@ -11,6 +11,6 @@ foreach ($fichiers as $fichier) {
     }
 }
 
-echo "Tous les fichiers ejpXX.txt ont été supprimés.\n";
+echo "Tous les fichiers ejpXX.txt ont Ã©tÃ© supprimÃ©s.\n";
 
 ?>
