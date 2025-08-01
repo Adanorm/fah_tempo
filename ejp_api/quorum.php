ed540ffa2aa567c93230d9ac3b26eedca4109697
JWhyFR <54511523+JWhyFR@users.noreply.github.com>
2025-08-01 09:04:01 +0200

    Update quorum.php
    refactoring pour n'utiliser que la boucle sur les $fichiers pour compter les occurrences.

--- ejp_api/quorum.php
+++ ejp_api/quorum.php
@@ -6,46 +6,35 @@ $ejp = array();
 // Utiliser glob pour trouver tous les fichiers correspondant au motif ejp*.txt
 $fichiers = glob('ejp*.txt');
 
+// Tableau pour compter les occurrences de chaque valeur
+$occ = array();
+
+// Variables pour trouver la valeur avec le plus grand nombre d'occurrences
+$consensus = null;
+$maxOccurrences = 0;
+
 // Parcourir chaque fichier trouvé
 foreach ($fichiers as $fichier) {
-    // Extraire XX du nom du fichier
-    $nomFichier = basename($fichier, '.txt'); // Supprimer l'extension .txt
-    $cle = substr($nomFichier, 3); // Extraire XX en supprimant 'ejp'
-
-    // Lire le contenu du fichier
+	// Lire le contenu du fichier
     $contenu = file_get_contents($fichier);
 
-    // Stocker le contenu dans le tableau associatif
-    $ejp[$cle] = $contenu;
+	// Compter les occurences d'un même contenu
+    if (isset($occ[$contenu])) {
+        $occ[$contenu]++;
+    } else {
+        $occ[$contenu] = 1;
+    }
+	// Identifier si c'est le nouveau max.
+    if ($occ[$contenu] > $maxOccurrences){
+        $maxOccurrences = $occ[$contenu];
+        $consensus = $contenu;
+	}
 }
 
 // Afficher le tableau associatif pour vérification
 echo "<pre>";
-print_r($ejp);
-echo "</pre>";
-
-// Tableau pour compter les occurrences de chaque valeur
-$occurrences = array();
-
-// Parcourir le tableau $ejp et compter les occurrences
-foreach ($ejp as $cle => $valeur) {
-    if (isset($occurrences[$valeur])) {
-        $occurrences[$valeur]++;
-    } else {
-        $occurrences[$valeur] = 1;
-    }
-}
-
-// Trouver la valeur avec le plus grand nombre d'occurrences
-$consensus = null;
-$maxOccurrences = 0;
-
-foreach ($occurrences as $valeur => $nombre) {
-    if ($nombre > $maxOccurrences) {
-        $maxOccurrences = $nombre;
-        $consensus = $valeur;
-    }
-}
+print_r($occ);
+echo "</pre>";			
 
 // Afficher la valeur de consensus dans la console
 echo "La valeur de consensus est : " . $consensus . "\n";
